52ef04c31d38cf8becc1b8d23d0bf8d4f9ceb983
SaadRamzan <112537643+msaadr@users.noreply.github.com>
2024-11-13 23:28:32 +0500

    small change

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     try {
-        res.send('Heello World!');
+        res.send('Heello World From Backend!');
     } catch (error) {
         console.error('Error in / route:', error);
         res.status(500).send('Internal Server Error');
